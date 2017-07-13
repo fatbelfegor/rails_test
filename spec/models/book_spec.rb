67d8e860800585a73dc1894d27297e9b4dee450a
Klim Agarkov <jewelgatorstv@gmail.com>
2017-07-13 17:25:13 +0300

    Cover Book model wit tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,5 +5,9 @@ describe Book  do
      it { should validate_presence_of(:title) }
      it { should validate_presence_of(:description) }
      it { should validate_presence_of(:cover_url) }
+
+     it { should validate_length_of(:title).is_at_most(80)  }
+     it { should validate_length_of(:description).is_at_most(240) }
+     it { should validate_length_of(:cover_url).is_at_most(120) }
   end
 end
