2cb70c251a119c7b3430c5d3418b93ede0e24ae6
Klim Agarkov <jewelgatorstv@gmail.com>
2017-07-13 14:16:30 +0300

    add CRUD for Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,28 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_permit)
+    if @book.save
+      flash[:success] = "New book added"
+      redirect_to book_path(@book)
+    else
+      flash[:error] = @book.errors.full_messages
+      redirect_to new_book_path
+    end
+  end
+
+  def book_permit
+    params.require(:Book).permit(:title, :description, :cover_url)
+  end
 end
