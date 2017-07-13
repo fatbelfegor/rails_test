d231d3f6de76c5e07c88ad7957f9f379d2ba10f0
Klim Agarkov <jewelgatorstv@gmail.com>
2017-07-13 19:26:11 +0300

    add BooksController GET#show test

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :book do
     title { Faker::Book.title }
-    description {Faker::Lorem.sentences(5) }
+    description {Faker::Lorem.sentences(3) }
     cover_url {Faker::Internet.url }
   end
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  let!(:book) { create(:book) }
   describe "GET #index" do
     before {get :index}
 
     it { should respond_with(200) }
+  end
+
+  describe "GET /books/:id" do
+    before { get :show, params: { id: book.id } }
+
+    context "When book exist" do
+      it "returns status code 200" do
+        expect(response.status).to eq(200)
+      end
+    end
   end
 end
